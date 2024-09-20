53dcf18354c268dcc7bfbf06983e74949de8ad30
Syed Haris shah <haris.official222@gmail.com>
2024-09-20 22:01:22 +0500

    node server created

--- myServer.mjs
+++ myServer.mjs
@@ -1,1 +1,13 @@
-console.log('hello world!')
+import { createServer } from 'node:http'
+
+const PORT = 3000
+
+const myServer = createServer((req, res)=>{
+
+    res.end('hello')
+
+})
+
+myServer.listen(PORT,()=>{
+    console.log(`port in listing on http://localhost:${PORT}`)
+})
