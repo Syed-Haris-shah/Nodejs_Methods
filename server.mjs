f6bfe5bf7226f69153e779e0de8ddd790933d3f3
Syed Haris shah <haris.official222@gmail.com>
2024-09-20 23:13:51 +0500

    created post method for testing data in postman

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import { createServer } from 'http';
 
 const PORT = 3000
 
-const server = createServer((req, res) => {
+const myServer = createServer((req, res) => {
 
     const METHOD = req.method;
     const URL = req.url;
@@ -18,20 +18,26 @@ const server = createServer((req, res) => {
             console.log('Data received:', parsedData);
             res.writeHead(201, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ message: 'Data received successfully!', data: parsedData}));
-            // try {
-            // } catch (error) {
-            //     res.writeHead(400, { 'Content-Type': 'application/json' });
-            //     res.end(JSON.stringify({ message: 'Invalid JSON' }));
-            // }
-           
         });
 
-    } else{
+    } else if (METHOD === 'POST' && URL === '/api/items'){
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const parsedData = JSON.parse(body);
+            console.log('Data received:', parsedData);
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Data received successfully!', data: parsedData}));
+        });
+ 
+    }else{
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Page Not Found!');
     }
 });
 
-server.listen(PORT, () => {
+myServer.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${3000}`);
 });
